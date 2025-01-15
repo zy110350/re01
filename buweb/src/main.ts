5bb16c9836162f75a9b42b27c6451daecae1d497
zy66 <zy_6661103@yeah.net>
2025-01-16 00:07:45 +0800

    提交2

--- buweb/src/main.ts
+++ buweb/src/main.ts
@@ -1,7 +1,7 @@
 import {createApp} from "vue";
 import App from "./App.vue";
 import pinia from "./store";
-import "./styles/reset.css";
+import "./styles/index.less";
 import router from "./router/router";                                                                                                                                                                                                                                                                                                                                                            
 const app=createApp(App);
 app.use(router);
--- buweb/vite.config.ts
+++ buweb/vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig, loadEnv } from "vite";
 import { viteMockServe } from "vite-plugin-mock";
 import type { UserConfig, ConfigEnv } from "vite";
 import { fileURLToPath } from "url";
+import AutoImport from 'unplugin-auto-import/vite';
+import Icons from 'unplugin-icons/vite';
+import Components from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import ElementPlus from 'unplugin-element-plus/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
@@ -30,6 +36,21 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
                 mockPath: 'mock', // 数据模拟需要拦截的请求起始 URL
                 enable: true // 本地环境是否开启 mock 功能
             }),
+            // 开启ElementPlus自动引入CSS
+            ElementPlus({}),
+            // 自动导入组件
+            AutoImport({
+                resolvers: [ElementPlusResolver(), IconsResolver()],
+                dts: fileURLToPath(new URL('./types/auto-imports.d.ts', import.meta.url))
+            }),
+            // 自动注册组件
+            Components({
+                resolvers: [ElementPlusResolver(), IconsResolver()],
+                dts: fileURLToPath(new URL('./types/components.d.ts', import.meta.url))
+            }),
+            Icons({
+                autoInstall: true
+            })
         ],
         // 运行后本地预览的服务器
         server: {
